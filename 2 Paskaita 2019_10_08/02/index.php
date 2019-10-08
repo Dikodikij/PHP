af12417f880b0d7231ca359eb9969583ab03a18e
Dikobriz <dikobriz@gmail.com>
2019-10-08 20:09:20 +0300

    PHP 04

---
+++ 2 Paskaita 2019_10_08/02/index.php
@@ -0,0 +1,17 @@
+<?php
+$cities = ['Berlynas', 'Roma', 'Londonas'];
+$cities[] = 'Tokijas';
+print_r($cities);
+
+echo "<ul>
+    <li> $cities[1]
+    </li>
+    </ul>";
+$cities2 = [
+    'Tokijas' => 13.6,
+    'Vasingtonas' => 0.6,
+    'Maskva' => 11.5
+];
+$cities2[] = 'Londonas => 8.6';
+echo "<ul>Gyventoju skaicius: $cities2[Tokijas] mln. </ul>"
+?>
